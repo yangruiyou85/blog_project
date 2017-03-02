041e456193e13b37c575bf4534c3d296809eefce
ruiyou.yang <ruiyou.yang@gmail.com>
2017-03-02 22:33:08 +0800

    register admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
+from models import *
+
+
 # Register your models here.
+
+
+
+
+admin.site.register(User)
+
+admin.site.register(Tag)
+
+admin.site.register(Article)
+
+admin.site.register(Category)
+
+admin.site.register(Comment)
+
+admin.site.register(Links)
+
+admin.site.register(Ad)
+
